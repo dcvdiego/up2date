ed4e834be694a1633201bb4b47f6e2d43f1e7d26
Diego Sergio Chuman Perez <diegochuman@gmail.com>
2022-02-13 14:57:26 +0000

    changed graphQL port and added description to search page

--- up2date/src/app/containers/FinderPage/index.tsx
+++ up2date/src/app/containers/FinderPage/index.tsx
@@ -28,18 +28,53 @@ const Title = styled.h1`
     `};
 `;
 
+const DescriptionContainer = styled.div`
+  margin-top: 2em;
+  ${tw`
+  w-full
+  max-w-screen-2xl
+  flex
+  justify-between
+  pl-3
+  pr-3
+  lg:pl-12
+  lg:pr-12
+  
+  `}
+`;
+
+const PageDescription = styled.p`
+  ${tw`
+  text-xs
+  lg:text-sm
+  xl:text-lg
+  sm:max-h-full
+  overflow-hidden
+  max-h-12
+  text-gray-800
+  `};
+`;
+
 export function FinderPage() {
   return (
     <PageContainer>
       <Navbar />
       <Title>Find the tutorial you need:</Title>
-      <Marginer direction="vertical" margin="4em" />
+      <DescriptionContainer>
+        <PageDescription>
+          You can use the Local Search to find tutorials uploaded directly to
+          this site (usually not found on YouTube) You can also search for
+          tutorials in YouTube directly and see if we have any up2date changes
+          for you!
+        </PageDescription>
+      </DescriptionContainer>
+      <Marginer direction="vertical" margin="6em" />
       <h3>Local Search</h3>
       <SearchBar source="Local" />
-      <Marginer direction="vertical" margin="4em" />
+      <Marginer direction="vertical" margin="8em" />
       <h3>Youtube Search</h3>
       <SearchBar source="Youtube" />
-      <Marginer direction="vertical" margin="4em" />
+      <Marginer direction="vertical" margin="8em" />
       <Footer />
     </PageContainer>
   );
